354f360b41b45883b34541cf639d912a065a876e
addiswan <s962925@student.knoxschools.org>
2026-02-18 15:38:36 +0000

    user input

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,2 +1,3 @@
-#Build a program that simulates a to-do list with saving/loading.
+#Build a program that simulates a to-do list with saving/loading
 
+user_input = print(input("Please type your To-Do list: "))
