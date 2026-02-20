b0e6d4653a83f4cb4b3a96e55615c2795cf0096c
addiswan <s962925@student.knoxschools.org>
2026-02-20 15:10:34 +0000

    deleted it

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,5 +1,4 @@
 #Build a program that simulates a to-do list with saving/loading
 to_do_list = {}
 def save_item():
-    question = input("What would you like to add to your To-Do list? ")
 initial_list = input("Please type your To-Do list: ")
