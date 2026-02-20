96c268e004d62fa488e8be8f1632594ed9951cb6
dezz72 <s1038294@student.knoxschools.org>
2026-02-20 15:09:54 +0000

    function and random variable (question)??

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,3 +1,5 @@
 #Build a program that simulates a to-do list with saving/loading
 to_do_list = {}
-_input = print(input("Please type your To-Do list: "))
+def save_item():
+    question = input("What would you like to add to your To-Do list? ")
+initial_list = input("Please type your To-Do list: ")
