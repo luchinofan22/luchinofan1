463b37d1fd49ca7164e5db76d771c0c2db1aba81
dezz72 <s1038294@student.knoxschools.org>
2026-03-18 13:34:51 +0000

    appendand return

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,5 +1,10 @@
 #Build a program that simulates a to-do list with saving/loading
-to_do_list = {}
+to_do_list = []
 initial_list = input("Please type your To-Do list: ")
+initial_list.split(",")
+to_do_list.append(initial_list)
+print(to_do_list)
 def save_item():
-    w
+    item = input("What item would you like to add to your to-do list?")
+    to_do_list.append(item)
+    return to_do_list
