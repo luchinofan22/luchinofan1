2ac3edbef273e4d58a792fbf96fc18f1e11db523
dezz72 <s1038294@student.knoxschools.org>
2026-03-20 13:40:48 +0000

    new function and returns

--- coding_challenge.py
+++ coding_challenge.py
@@ -4,7 +4,11 @@ initial_list = input("Please type your To-Do list: ")
 initial_list.split(",")
 to_do_list.append(initial_list)
 print(to_do_list)
-def save_item():
+def save_list(your_list_here):
+    return to_do_list == your_list_here
+
+def add_item(list_b):
     item = input("What item would you like to add to your to-do list?")
-    to_do_list.append(item)
-    return to_do_list
+    list_b.append(item)
+    save_list(list_b)
+    return list_b
